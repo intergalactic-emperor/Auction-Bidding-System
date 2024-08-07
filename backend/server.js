2c292a647732829a0a5134b57b22dce5cd23b757
intergalactic-emperor <165413014+intergalactic-emperor@users.noreply.github.com>
2024-08-07 18:19:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,5 +22,4 @@ app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/auctions", require("./routes/auctionRoutes"));
 app.use("/api/bids", require("./routes/bidRoutes"));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
